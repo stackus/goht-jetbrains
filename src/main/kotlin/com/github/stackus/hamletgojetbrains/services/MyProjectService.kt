101d6208417b24df5ef4e14335ccff59082566ce
GitHub Action <action@github.com>
2024-01-13 17:17:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/stackus/hamletgojetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stackus/hamletgojetbrains/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.stackus.hamletgojetbrains.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stackus.hamletgojetbrains.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
